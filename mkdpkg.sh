5375816a4bea02b6b8353a677220c15f61034b72
WerIstLuka <WerIstLuka@gmail.com>
2025-12-13 17:01:03 +0100

    added mkdpkg.sh and changed debian files

---
+++ mkdpkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir package
+cp -r usr/ package
+cp -r DEBIAN package
+dpkg-deb --root-owner-group --build "package" > /dev/null
+mv package.deb 20.3-Mint-Y-Themes.deb
+rm -rf package
